150c2f7e5a752049814969f77489a604a72b3421
Feisal Mohamed <feisalmib@gmail.com>
2022-10-06 03:12:09 +0300

    updates

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,17 @@
--- prepares a MySQL server for the project
+-- MySQL server
+-- tests should pass even if they do exist (DB and tables)
 
+-- create database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create a new user hbnb_dev (in localhost)
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+
+-- grant hbnb_dev all privilege on hbnb_dev_db database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- grant hbnb_dev SELECT privilege on performance_schema database
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- enables smooth refresh
 FLUSH PRIVILEGES;
